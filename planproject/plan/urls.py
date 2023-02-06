38aff00eba88f045355ab48b6012f637b5820072
elizabathsneha <snehaelizabathsneha@gmail.com>
2023-02-06 14:33:55 +0530

    added

---
+++ planproject/plan/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path,include
+from  . import views
+
+
+
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('cbvhome/',views.TaskListview.as_view(),name='cbvhome'),
+    path('cbvdetails/<int:pk>/',views.TaskDetailview.as_view(),name='cbvdetails'),
+    path('cbvupdate/<int:pk>/',views.TaskUpdateView.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.TaskDeleteView.as_view(),name='cbvdelete'),
+
+]
+
